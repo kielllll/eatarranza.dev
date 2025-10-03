de6a36c489c23d5576c97c80cd1e30e098513b83
kielllll <eatarranza@gmail.com>
2025-10-04 00:00:40 +0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
-  integrations: [react()],
+  integrations: [react(), icon()],
 });
